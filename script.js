09b9122bdff020d66e7385df6ccc883e164317fa
Akshaya Chelliah <72297207+Akshaya-vc@users.noreply.github.com>
2021-01-01 18:25:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var element = document.getElementById('navbar-height-responsive');
+    element.classList.toggle('navbar-height-normal');
+   document.getElementsByClassName('but')[0].style.opacity="1";
+   document.getElementsByClassName('but')[1].style.opacity="1";
+   document.getElementsByClassName('but')[2].style.opacity="1";
+   document.getElementsByClassName('but')[3].style.opacity="1";
+}
